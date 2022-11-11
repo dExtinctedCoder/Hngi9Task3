d7f367fb71aa34f3956dc21f078674f347cf6956
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-11 10:09:58 +0100

    code: create necessary file and folder

---
+++ src/components/hooks/useFetchItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const useFetchItem = () => {
+  return (
+    <div>useFetchItem</div>
+  )
+}
+
+export default useFetchItem
---
+++ src/pages/place_to_stay.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const place_to_stay = () => {
+  return (
+    <div>place_to_stay</div>
+  )
+}
+
+export default place_to_stay
