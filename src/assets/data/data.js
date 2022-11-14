31db66159c56099967ce2aa7bc429415bfc67d1c
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-14 19:30:45 +0100

    style: style file for home page

---
+++ src/assets/data/data.js
@@ -0,0 +1,36 @@
+import IMG1 from '../media/img1.png'
+import IMG2 from '../media/img2.png'
+import IMG3 from '../media/img3.png'
+import IMG4 from '../media/img4.png'
+import IMG5 from '../media/img5.png'
+import IMG6 from '../media/img6.png'
+import IMG7 from '../media/img7.png'
+import IMG8 from '../media/img8.png'
+import IMG9 from '../media/img9.png'
+import IMG10 from '../media/img10.png'
+import IMG11 from '../media/img11.png'
+import IMG12 from '../media/img12.png'
+import IMG13 from '../media/img13.png'
+import IMG14 from '../media/img14.png'
+import IMG15 from '../media/img15.png'
+import IMG16 from '../media/img16.png'
+
+
+export const ImgSrc = [
+  {src: IMG1},
+  {src: IMG2},
+  {src: IMG3},
+  {src: IMG4},
+  {src: IMG5},
+  {src: IMG6},
+  {src: IMG7},
+  {src: IMG8},
+  {src: IMG9},
+  {src: IMG10},
+  {src: IMG11},
+  {src: IMG12},
+  {src: IMG13},
+  {src: IMG14},
+  {src: IMG15},
+  {src: IMG16},
+]
