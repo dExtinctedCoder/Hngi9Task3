c09494f45992fef1672eb4ffd13b4e2cfb1a84ee
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-14 19:44:36 +0100

    refactor: restructure file to include all necessary components for the home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import './home.css'
+import Nav from '../components/nav/Nav'
+import Hero from '../components/hero/Hero'
+import Footer from '../components/footer/Footer'
+import MbToken from '../assets/media/Group 59537.svg'
+import MetaMask from '../assets/media/Group 4040.svg'
+import OpenSea from '../assets/media/Frame 4041.svg'
+import Button from '../components/button/Button'
+import Frame from '../assets/media/Frame 59546.png'
+import Nfts from '../components/nfts/Nfts'
 
 const home = () => {
   return (
-    <div>home</div>
+    <>
+      <Nav />
+      <Hero />
+      <div className="ecosystem">
+        <div><img src={MbToken} /></div>
+        <div><img src={MetaMask} /></div>
+        <div><img src={OpenSea} /></div>
+      </div>
+      <main>
+        <h3>Inspiration for your next adventure</h3>
+        <Nfts minItemsNo={0} maxItemsNo={8}/>
+      </main>
+      <section className="community">
+        <div className="community-left">
+          <h3>Metabnb NFTs</h3>
+          <p>Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.</p>
+          <Button subText={"Learn more"} />
+        </div>
+        <div className="community-right">
+          <img src={Frame} alt="" />
+        </div>
+      </section>
+      <Footer />
+    </>
   )
 }
 
