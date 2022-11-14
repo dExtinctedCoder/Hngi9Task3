75f759b9086efed3ddb5f9e59bf4f1e303994a98
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-14 19:28:00 +0100

    refactor: modify root App file to manage necessary assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
+import { createContext, useState } from 'react'
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../src/pages/home'
+import Modal from '../src/components/modal/Modal'
+import Menu from '../src/components/menu/Menu'
 import PlaceToStay from '../src/pages/place_to_stay'
+
+export const ModalContext = createContext()
+
+// const initState = 
 
 function App() {
 
+  const [modalState, setModalState] = useState({
+    isModalOpen: false,
+    setBodyPosition: false
+  })
+  const [menuState, setMenuState] = useState(false)
+  
   return (
-    <div className="page__control">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/place" element={<PlaceToStay />} />
-      </Routes>
-    </div>
+    <ModalContext.Provider value={{modalState, setModalState, menuState, setMenuState}}>
+      <div className="page__control">
+        <Modal />
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="place" element={<PlaceToStay />} />
+        </Routes>
+      </div>
+    </ModalContext.Provider>
   )
 }
 
